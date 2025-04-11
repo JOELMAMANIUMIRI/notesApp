111046510f2b3173a4ca7ed42ab787ecf8de2288
JOELMAMANIUMIRI <joelmani9516@gmail.com>
2025-04-11 20:07:55 -0300

    Inclusao do projeto NotesApp

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace NotesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+}
